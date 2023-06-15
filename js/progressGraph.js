a7136f4d2a26af48d7d9e85ebeef7f5787b9a75c
Amit Perets <106886074+AmitPerets@users.noreply.github.com>
2023-06-15 20:58:38 +0300

    Update progressGraph.js

--- js/progressGraph.js
+++ js/progressGraph.js
@@ -2,18 +2,15 @@
   var user = await callJson("db.getUserName"); // Specify the user for which you want to display sessions
   var userSessions = await callJson("db.getUserSessions");
 
-  // Retrieve the sessions for the specific user
-  var specificUserSessions = userSessions[user] || [];
-
   // Create a chart using Chart.js
   var ctx = document.getElementById('sessionChart').getContext('2d');
   var chart = new Chart(ctx, {
-    type: 'bar',
+    type: 'line',
     data: {
       labels: user,
       datasets: [{
         label: 'Sessions',
-        data: specificUserSessions,
+        data: userSessions,
         backgroundColor: 'rgba(75, 192, 192, 0.5)',
         borderColor: 'rgba(75, 192, 192, 1)',
         borderWidth: 1
