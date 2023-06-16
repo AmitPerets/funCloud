982e1d3b86c7fe4378a72ba53928e24765d6d21c
Amit Perets <106886074+AmitPerets@users.noreply.github.com>
2023-06-16 11:46:10 +0300

    Update progressGraph.js

--- js/progressGraph.js
+++ js/progressGraph.js
@@ -1,25 +1,22 @@
 (async function gen() {
   var user = await call("db.getUserName"); // Specify the user for which you want to display sessions
   var userSessions = await callJson("db.getUserSessions");
-
+  console.log(userSessions.map((s, i) => { return i }));
   // Create a chart using Chart.js
   var ctx = document.getElementById('sessionChart').getContext('2d');
   var chart = new Chart(ctx, {
     type: 'line',
     data: {
+      labels: userSessions.map((s, i) => { return "session " + i+1 }), // Corrected property name
       datasets: [{
-        label: user+"'s sessions:",
+        label: user + "'s sessions:",
         data: userSessions,
+        fill: false,
         borderColor: 'rgb(75, 192, 192)',
-        borderWidth: 1
+        tension: 0.1
       }]
     },
     options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      },
       plugins: {
         legend: {
           labels: {
@@ -36,9 +33,9 @@
 
 async function back() {
   await call('clear')
-    var user = await callJson("db.getUser");
-    console.log(user);
-    view_page("main", user);
+  var user = await callJson("db.getUser");
+  console.log(user);
+  view_page("main", user);
 }
 
 function logout() {
